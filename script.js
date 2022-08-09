e79f60d78b522bfd1978ef6840cfed072c38fd6a
yukiuchima <yuema@asu.edu>
2022-08-08 21:10:06 -0700

    for loop added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let addBtn = document.querySelector('.addToBtn');
+let addBtns = document.querySelectorAll('.addToBtn');
 let inputField = document.querySelector('.inputField');
 var demovalue;
 
@@ -11,24 +11,27 @@ $(document).ready(function(){
     });
 });
 
+for (addBtn of addBtns){
+    addBtn.addEventListener('click', function(){
+        let toDoContainer = document.querySelector('#toDoContainer'+demovalue);
+        console.log(demovalue);
+        let newItem = document.createElement('p');
+        newItem.classList.add('paragraph-styling');
+        //Take text value from inputField
+        newItem.innerText = inputField.value;
+        //Add the newItem to the container list
+        toDoContainer.appendChild(newItem);
+        //Remove item that was in the input field
+        inputField.value = "";
+    
+        //Detect when item is clicked
+        newItem.addEventListener('click', function(){
+            newItem.style.textDecoration = "line-through";
+            setTimeout(() => {
+                toDoContainer.removeChild(newItem);
+            }, 1000);
+        });
+    })
+}
 
-addBtn.addEventListener('click', function(){
-    let toDoContainer = document.querySelector('#toDoContainer'+demovalue);
-    let newItem = document.createElement('p');
-    newItem.classList.add('paragraph-styling');
-    //Take text value from inputField
-    newItem.innerText = inputField.value;
-    //Add the newItem to the container list
-    toDoContainer.appendChild(newItem);
-    //Remove item that was in the input field
-    inputField.value = "";
 
-    //Detect when item is clicked
-    newItem.addEventListener('click', function(){
-        newItem.style.textDecoration = "line-through";
-        setTimeout(() => {
-            toDoContainer.removeChild(newItem);
-        }, 1000);
-    });
-
-})
